c9b9614c821be78160212854181aad900cf48903
rferpae569 <rferpae569@g.educaand.es>
2025-09-30 21:55:38 +0200

    PokeInfo Rubén Fernández

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
-import Types from "./pages/Types";
-import Gyms from "./pages/Gyms";
-import Leagues from "./pages/Leagues";
-import Pokedex from "./pages/Pokedex";
 
 export default function App() {
   return (
@@ -14,10 +10,6 @@ export default function App() {
       <main className="min-h-screen">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/types" element={<Types />} />
-          <Route path="/gyms" element={<Gyms />} />
-          <Route path="/leagues" element={<Leagues />} />
-          <Route path="/pokedex" element={<Pokedex />} />
         </Routes>
       </main>
       <Footer />
